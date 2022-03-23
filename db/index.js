3330d064372ee7f7de5aa2b5079c8ed789f38587
Alejandro <alexsagarnaga@gmail.com>
2022-03-23 14:24:58 -0500

    worked on pt2 posts new table new functions pending getuserbyid

--- db/index.js
+++ db/index.js
@@ -5,34 +5,60 @@ const client = new Client("postgres://localhost:5432/juicebox-dev");
 
 async function getAllUsers() {
   const { rows } = await client.query(
-    `SELECT id, username
+    `SELECT id, username, name, location
         FROM users;`
   );
 
   return rows;
 }
 
-async function createUser({ username, password }) {
+async function createUser({ username, password, name, location }) {
   try {
-    const result = await client.query(
+    const { rows } = await client.query(
       `
-        INSERT INTO users(username, password) 
-        VALUES ($1, $2)
+        INSERT INTO users(username, password, name, location) 
+        VALUES ($1, $2, $3, $4)
         ON CONFLICT (username) DO NOTHING
         RETURNING *;
         `,
-      [username, password]
+      [username, password, name, location]
     );
-    const { rows } = await client.query(
-        `SELECT id, username
-            FROM users;`
-      );
-    
-      return rows;
 
+    return rows;
   } catch (error) {
     throw error;
   }
 }
 
-module.exports = { client, getAllUsers, createUser };
+async function updateUser(id, fields = {}) {
+  // build the set string
+
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}" =$${index + 1}`)
+    .join(", ");
+
+  // return early if this is called without fields
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+  UPDATE users
+  SET ${setString}
+  WHERE id=${id}
+  RETURNING *;
+  `,
+      Object.values(fields)
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { client, getAllUsers, createUser, updateUser };
--- db/seed.js
+++ db/seed.js
@@ -1,9 +1,10 @@
-const { client, getAllUsers, createUser } = require("./index");
+const { client, getAllUsers, createUser, updateUser } = require("./index");
 
 async function dropTables() {
   try {
     console.log("Starting to drop tables...");
     await client.query(`
+            DROP TABLE IF EXISTS posts;
             DROP TABLE IF EXISTS users;
        `);
     console.log("Finished dropping tables");
@@ -19,8 +20,18 @@ async function createTables() {
             CREATE TABLE users (
                 id SERIAL PRIMARY KEY,
                 username varchar(255) UNIQUE NOT NULL,
-                password varchar(255) NOT NULL
+                password varchar(255) NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                location VARCHAR(255) NOT NULL,
+                active BOOLEAN DEFAULT true
             );
+            CREATE TABLE posts (
+              id SERIAL PRIMARY KEY,
+              "authorId" INTEGER REFERENCES users(id) NOT NULL,
+              title VARCHAR(255) NOT NULL,
+              content TEXT NOT NULL,
+              active BOOLEAN DEFAULT true
+          );
        `);
     console.log("Finished building tables");
   } catch (error) {
@@ -35,19 +46,25 @@ async function createInitialUsers() {
     const albert = await createUser({
       username: "albert",
       password: "bertie99",
+      name: "alex",
+      location: "tx",
     });
-/*    const albertTwo = await createUser({
+    /*    const albertTwo = await createUser({
       username: "albert",
       password: "imposter_albert",
     }); */
     const sandra = await createUser({
-        username: "sandra",
-        password: "2sandy4me",
-      });
-      const glamgal = await createUser({
-        username: "glamgal",
-        password: "soglam",
-      });    
+      username: "sandra",
+      password: "2sandy4me",
+      name: "alex",
+      location: "tx",
+    });
+    const glamgal = await createUser({
+      username: "glamgal",
+      password: "soglam",
+      name: "alex",
+      location: "tx",
+    });
 
     console.log("Finished creating users");
   } catch (error) {
@@ -70,12 +87,22 @@ async function rebuildDB() {
 async function testDB() {
   try {
     //connects client to db
-    //client.connect()
-    //queries are promises, so need to await
+    // client.connect()
+    // queries are promises, so need to await
     const users = await getAllUsers();
-    const createdUser = await createUser({username: "vince", password: "5"})
+    const createdUser = await createUser({
+      username: "vince",
+      password: "5",
+      name: "alex",
+      location: "TX",
+    });
     console.log("getAllUsers:", users);
     console.log("createdUser:", createdUser);
+    const updateUserResult = await updateUser(4, {
+      name: "Newname New",
+      location: "NYC, NY",
+    });
+    console.log("Update User Result", updateUserResult);
     console.log("Finished database tests");
   } catch (error) {
     console.error("Error testing database");
