06eaa865560ba0bf44b4bbdc413f14773f50575a
Alejandro <alexsagarnaga@gmail.com>
2022-03-29 14:28:25 -0500

    started part 3 of module

--- api/posts.js
+++ api/posts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const postsRouter = express.Router();
+const { requireUser } = require('./utils');
 const { getAllPosts } = require("../db");
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
@@ -12,6 +13,10 @@ postsRouter.get("/", async (req, res) => {
   console.log("I AM RUNNING");
 
   res.send(posts);
+});
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  res.send({ message: 'under construction' });
 });
 
 module.exports = postsRouter;
